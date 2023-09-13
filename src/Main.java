b45dd22abc53c2e09a6243fb39d7c73901599811
Xphsl <stevenl2026@student.cis.edu.hk>
2023-09-13 09:58:23 +0800

    Quiz 1.1

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
+        Scanner s = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favoriteFood;
+        System.out.println("Enter.age");
+        age= s.nextInt();
+        s.nextLine();
+        System.out.println("Enter your first name");
+        firstName=s.nextLine();
+        System.out.println("Enter your favorite food");
+        favoriteFood=s.nextLine();
+        System.out.println(age+firstName+favoriteFood);
 
     }
 
